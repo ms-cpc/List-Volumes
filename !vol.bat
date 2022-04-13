68d3283f14f0146b62b4ea52a4078c03451ca42f
MS <52162055+ms-cpc@users.noreply.github.com>
2022-04-13 11:02:08 -0400

    Add files via upload

---
+++ !vol.bat
@@ -0,0 +1,30 @@
+@echo off
+vol a: >vol!.txt
+vol b: >>vol!.txt
+vol c: >>vol!.txt
+vol d: >>vol!.txt
+vol e: >>vol!.txt
+vol f: >>vol!.txt
+vol g: >>vol!.txt
+vol h: >>vol!.txt
+vol i: >>vol!.txt
+vol j: >>vol!.txt
+vol k: >>vol!.txt
+vol l: >>vol!.txt
+vol m: >>vol!.txt
+vol n: >>vol!.txt
+vol o: >>vol!.txt
+vol p: >>vol!.txt
+vol q: >>vol!.txt
+vol r: >>vol!.txt
+vol s: >>vol!.txt
+vol t: >>vol!.txt
+vol u: >>vol!.txt
+vol v: >>vol!.txt
+vol w: >>vol!.txt
+vol x: >>vol!.txt
+vol y: >>vol!.txt
+vol z: >>vol!.txt
+cls
+type vol!.txt |more
+pause
